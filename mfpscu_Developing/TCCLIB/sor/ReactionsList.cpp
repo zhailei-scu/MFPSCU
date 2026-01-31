b04e5c40086eabc0279c43e0a253b679f951048c
zhailei-scu <zhaileiytp@163.com>
2026-01-31 21:02:32 +0800

    add file header of authors information for cpp files

--- mfpscu_Developing/TCCLIB/sor/ReactionsList.cpp
+++ mfpscu_Developing/TCCLIB/sor/ReactionsList.cpp
@@ -1,3 +1,13 @@
+/*********************************************************************************
+!--- Description:
+!--- Author : Qing Hou, Insti. of Nucle. Sci. and Tech., Sichuan University
+!--- Email : qhou@scu.edu.cn
+!--- data: From 2017-09 to 2021-12
+!--- License: MIT License (There are no limitation for anyone to use/modify/sale of this code, be happy to use this code)
+!--- Please ref "Lei Zhai, Chaoqiong Ma, Jiechao Cui and Qing Hou, GPU-based acceleration of Monte Carlo simulations for migration-coalescence evolution of gas bubbles in materials
+!---                        Modelling Simul. Mater. Sci. Eng. 2019. 27 055008,https://iopscience.iop.org/article/10.1088/1361-651X/ab1d14
+!**********************************************************************************/
+
 #include "ReactionsList.h"
 #ifdef __cplusplus
 #include <iostream>
