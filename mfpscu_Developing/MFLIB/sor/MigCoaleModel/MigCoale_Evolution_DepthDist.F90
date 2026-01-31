f013d9e97ce0efb2be3a461cf1558e64b761f364
zhailei-scu <zhaileiytp@163.com>
2026-01-31 20:53:57 +0800

    add file header of authors information

--- mfpscu_Developing/MFLIB/sor/MigCoaleModel/MigCoale_Evolution_DepthDist.F90
+++ mfpscu_Developing/MFLIB/sor/MigCoaleModel/MigCoale_Evolution_DepthDist.F90
@@ -1,3 +1,12 @@
+!*********************************************************************************!
+!--- Description:
+!--- Author : Lei Zhai, Insti. of Nucle. Sci. and Tech., Sichuan University
+!--- Email : zhaileiytp@163.com
+!--- data: From 2017-09 to 2021-12
+!--- License: MIT License (There are no limitation for anyone to use/modify/sale of this code, be happy to use this code)
+!--- Please ref "Lei Zhai, Chaoqiong Ma, Jiechao Cui and Qing Hou, GPU-based acceleration of Monte Carlo simulations for migration-coalescence evolution of gas bubbles in materials
+!---                        Modelling Simul. Mater. Sci. Eng. 2019. 27 055008,https://iopscience.iop.org/article/10.1088/1361-651X/ab1d14
+!*********************************************************************************!
 module MIGCOALE_EVOLUTION_DEPTHDIST
     use MCMF_CONSTANTS
     contains
